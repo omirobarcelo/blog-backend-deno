1e69658c90a3f88fd60eff17345c8e5c4b7b70b7
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-06-02 11:52:48 +0200

    Updated Log middleware

--- app/middlewares/log.middleware.ts
+++ app/middlewares/log.middleware.ts
@@ -1,25 +1,29 @@
 import { blue, cyan, white, yellow } from '../../deps.ts';
-import { Context, Middleware, MiddlewareTarget } from '../../deps.ts';
+import { Context, HttpError, Middleware, MiddlewareTarget } from '../../deps.ts';
+import { Logger } from '../common/utils/logger.ts';
 
 @Middleware(new RegExp('/'))
 export class Log implements MiddlewareTarget<unknown> {
   onPreRequest(context: Context<unknown>) {
     return new Promise((resolve, reject) => {
-      const printMethod = `${yellow('[')}${white(context.request.method)}${yellow(']')}`;
-      // const printStatus = cyan(res.status);
-      const httpVersion = cyan(context.request.serverRequest.proto);
-      const localAddr = context.request.serverRequest.conn.localAddr as any;
-      const ip = cyan(`${localAddr?.hostname}:${localAddr?.port}`);
-      const endpoint = `[${white('route:')} ${blue(context.request.url)}]`;
-      console.log(
-        `${printMethod} ${white('|')} ${httpVersion} ${white('|')} ${ip} ${endpoint}`
-      );
       resolve();
     });
   }
 
   onPostRequest(context: Context<unknown>) {
     return new Promise((resolve, reject) => {
+      const printMethod = `${yellow('[')}${white(context.request.method)}${yellow(']')}`;
+      const status = context.response.error
+        ? (context.response.error as HttpError).httpCode || 500
+        : context.response.status
+        ? context.response.status
+        : 200;
+      const printStatus = cyan(status.toString(10));
+      const httpVersion = cyan(context.request.serverRequest.proto);
+      const localAddr = context.request.serverRequest.conn.localAddr as any;
+      const ip = cyan(`${localAddr?.hostname}:${localAddr?.port}`);
+      const endpoint = `[${white('route:')} ${blue(context.request.url)}]`;
+      Logger.log(`${printMethod} ${printStatus} ${white('|')} ${httpVersion} ${white('|')} ${ip} ${endpoint}`);
       resolve();
     });
   }
