ba7a4c5caa4f7183b5962ec22f7f19299c7aefb2
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-06-02 14:49:09 +0200

    Pass appropriate request object

--- app/areas/posts/posts.service.ts
+++ app/areas/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { join, normalize } from '../../../deps.ts';
 import { readJson } from '../../../deps.ts';
-import { Response, serveFile } from '../../../deps.ts';
+import { Request, Response, serveFile } from '../../../deps.ts';
 import { Post } from './interfaces/post.interface.ts';
 
 export class PostsService {
@@ -22,8 +22,8 @@ export class PostsService {
    * @param id 
    * @param req Request that serves the file
    */
-  async getPost(id: string, req: any): Promise<Response> {
+  async getPost(id: string, req: Request): Promise<Response> {
     const postLocations = (await readJson(`${this.CWD}/post_loc.json`)) as { [id: string]: string };
-    return serveFile(req, normalize(join(this.CWD, postLocations[id])));
+    return serveFile(req.serverRequest as any, normalize(join(this.CWD, postLocations[id])));
   }
 }
